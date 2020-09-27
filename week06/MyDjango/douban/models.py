977872afaff7c93fb5ccf72f6746a691132e4d06
heyu <shy19940918@live.com>
2020-09-28 01:23:54 +0800

    第六周作业提交

--- week06/MyDjango/douban/models.py
+++ week06/MyDjango/douban/models.py
@@ -78,6 +78,7 @@ class Comment(models.Model):
     film_name = models.CharField(max_length=255, blank=True, null=True)
     content = models.CharField(max_length=2000, blank=True, null=True)
     star = models.IntegerField(blank=True, null=True)
+    comment_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         managed = False
--- week06/MyDjango/douban/views.py
+++ week06/MyDjango/douban/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Comment
+
+
+def books_short(request):
+    content = ''
+    queryset = Comment.objects.all()
+    conditions = {'star__gt': 3}
+    comments = queryset.filter(**conditions)
+    print(comments)
+    return render(request, 'index.html', locals())
+
+
+def books_short_filter(request, content):
+    queryset = Comment.objects.all()
+    conditions = {'star__gt': 3, 'content__contains': content}
+    comments = queryset.filter(**conditions)
+    return render(request, 'index.html', locals())
